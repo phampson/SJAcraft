9f6dc4f42fff7b1a0dccb58f49938a6b7e7e855b
Ubuntu <ubuntu@ip-172-26-11-121.us-west-2.compute.internal>
2017-10-13 23:45:01 +0000

    create a bew file

---
+++ login/multi/client.php
@@ -0,0 +1,14 @@
+<?php
+    if (!extension_loaded('curl')) {
+        echo 'failed';
+    } else {
+        echo 'loaded';
+    }
+error_reporting(E_ALL); ini_set('display_errors', '1');
+$name = "XU";
+$url ="http://34.214.129.0/login/multi/index.php?name=XU";
+$client=curl_init($url);
+curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
+$response=curl_exec($client);
+echo $response;
+?>
