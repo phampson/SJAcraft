a6434cc1915cf281cf935c1a257f6671ba8041fd
Ubuntu <ubuntu@ip-172-26-13-143.us-west-2.compute.internal>
2017-10-19 22:04:26 +0000

    added navbar folder with navbar and indexnavbar

--- dlc/dlc.php
+++ dlc/dlc.php
@@ -11,38 +11,10 @@
 <body>
 
 <!-- Nav Bar -->
-<nav class="navbar navbar-inverse">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="../index.html">WarCraft II</a>
-		</div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li><a href="index.html">About</a></li>
-				<li class="dropdown active">
-					<a class="dropdown-toggle" data-toggle="dropdown"href="#">Download
-					<span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Download the game</a></li>
-						<li><a href="#">Download maps</a></li>
-					</ul>
-				</li>
-				<li><a href="#">FAQ</a></li>
-				<li><a href="#">Forum</a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#"> Sign Up</a></li>
-				<li><a href="../login/login.html"><span class="glyphicon glyphicon-user"></span> Log In</a></li>
-			</ul>
-		</div>
-	</div>
-</nav>
+<div id="navbar"></div>
+<script>
+        $("#navbar").load("../navbar/navbar.html")
+</script>
 
 <!-- CDN gallery -->
 <h2 style="color: white; text-align: center;">Map Gallery</h2>
--- forum/forum.php
+++ forum/forum.php
@@ -21,38 +21,10 @@ else{echo"nothing";}
 <body>
 
 <!-- Nav Bar -->
-<nav class="navbar navbar-inverse">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="../index.html">WarCraft II</a>
-		</div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li><a href="#">About</a></li>
-				<li class="">
-					<a class="dropdown-toggle" data-toggle="dropdown"href="#">Download
-					<span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="../downloadthegame/downloadthegame.html">Download the game</a></li>
-						<li><a href="../dlc/dlc.php">Download maps</a></li>
-					</ul>
-				</li>
-				<li><a href="../faq/faq.html">FAQ</a></li>
-				<li class="active"><a href="#">Forum</a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="../signup/signup.html"> Sign Up</a></li>
-				<li><a href="../login/login.html"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
-			</ul>
-		</div>
-	</div>
-</nav>
+<div id="navbar"></div>
+<script>
+        $("#navbar").load("../navbar/navbar.html")
+</script>
 
 <div>
 	<h1>Forums</h1>
