38235e108b01f247e0bb2b4824d802a4225308a3
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-10-22 15:50:21 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+include('login/start.php');
+
+error_reporting(E_ALL); ini_set('display_errors', '1');
+session_start();
+if(isset($_SESSION['user_id'])){
+	echo $_SESSION['user_id'];
+	$sql = 'select * from user_info where username="'.$_SESSION['user_id'].'"';
+	$query = $mysqli->query($sql);
+	if($query) {
+		$fetch = $query->fetch_assoc();
+		$username = $fetch['username'];
+		$email = $fetch['email'];
+		$avatarPath = $fetch['avatar_path'];
+		echo $email;
+		$navpath = "navbar/navbarlogged.html";
+	}
+}
+else{
+	echo"nothing";
+	$username = "username unknown";
+	$email = "email unknown";
+	$navpath = "navbar/navbar.html";
+	echo "\n" . $navpath;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Warcraft II</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="stylesheet.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- Nav Bar -->
+<div id="navbar"></div>
+<?php
+
+echo "<script>\n";
+        echo '$("#navbar").load("' . $navpath . '")';
+echo "</script>\n";
+?>
+
+<!-- Banner -->
+<img src="img/SplashMirror.png" width="1250" class="center-block">
+
+
+<!-- Logo -->
+<img src="img/Logo.png" width="400" class="logo" style="position: relative; left: calc(50% - 200px); top: -480px;">
+
+<!-- Play Button -->
+<button type="button" class="btn btn-play" style="position: relative; left: calc(50% - 480px); top: -300px;">Play Now</button>
+
+<!-- Leader Board -->
+<div class="container col-sm-8 col-sm-offset-2" style="background-color: white; position: relative; top: -200px;">
+	<h1>Leader Board</h1>
+	<h4>1. Parzival</h4>
+	<h4>2. Art3mis</h4>
+	<h4>3. Aech</h4>
+	<h4>4. Daito</h4>
+	<h4>5. Shoto</h4>
+	<h4>6. IOI-655321</h4>
+	<h4>7. IOI-655892</h4>
+	<h4>8. IOI-650945</h4>
+	<h4>9. IOI-675333</h4>
+	<h4>10. IOI-630041</h4>
+</div>
+
+</body>
+</html>
