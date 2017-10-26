4e8cf5159e77cd5f0dcf2e96f788f5467e5387f0
alice-huang <auang@ucdavis.edu>
2017-10-26 11:05:08 -0700

    changed page layour of profile

--- profile/profile.php
+++ profile/profile.php
@@ -35,7 +35,7 @@ else {
 	html, body {
 	background: url("../img/Texture.png") repeat;} 
 
-	.profile {
+		.profile {
 		position:absolute;
 		top: 50%;
 		left: 50%;
@@ -45,6 +45,7 @@ else {
 		height: 500px;
 		background: rgba(0,0,0,0.4);
 		z-index: -999;
+
 	}
 
 	.profilePic{
@@ -54,40 +55,58 @@ else {
 		float:left;
 	}
 
+.button {
+    background-color: #e7e7e7;
+    border: none;
+    color: black;
+    padding: 8px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 18px;
+    margin: 4px 10px;
+    cursor: pointer;
+}
+
+
 
 	.profile username {
 		position:absolute;
 		top: 30px;
-		left: 300px;
+		left: 400px;
 		color: white;
 		font-size: 40px;
-	}
 
+
+	}
+	.username{ 
+		position: static;
+	}
 
 	.profile email{
 		position:absolute;
-		top: 0%;
-		left: 0%;	
-		width: 350px;
-		height: 80px;
-		background: rgba(220, 220, 171, 0.74);
+		top: 90px;
+		left: 400px;	
 		font-size: 18px;
 		text-align: center;
+		color: white;
+		font-size: 35px;
+	}
+	.email{
+		position:static;
 	}
 
-	.profile addFriend {
-		position:relative;
-		left: -170px;
+	.addFriend {
+		position:absolute;
+		left: 500px;
 		top: 200px;
-		color: gray;
 		font-size: 20px;
 	}
 
-	.profile addFriend2 {
-		position:relative;
-		left: 320px;
-		top: 200px;
-		color: gray;
+	.message {
+		position:absolute;
+		left: 500px;
+		top: 250px;
 		font-size: 20px;
 	}
 
@@ -118,11 +137,11 @@ echo "</script>\n";
 	<!--profile picture-->
 	<?php
 	echo "<div class='profilePic', id='profilePic'>
-		 <img src=$avatarPath alt='This is where your profile picture goes' style='width:200px;height:200px;'>";
+		 <img src=$avatarPath alt='This is where your profile picture goes' style='width:300px;height:300px;'>";
 	?>   
 	<form action="upload.php" method="post" enctype="multipart/form-data">
-	    <upload>Select image to upload:</upload>
-	    <input type="file" name="fileToUpload" id="fileToUpload">
+	    <upload> <font color ="white" >Select image to upload:</upload>
+	    <input type="file" name="fileToUpload" id="fileToUpload"></font>
 	    <input type="submit" value="Upload Image" name="submit">
 	</form>
 
@@ -130,10 +149,11 @@ echo "</script>\n";
 	</div>
 
 	
-		<username><?php echo $username; ?>Temp bc doesn't work</username>
-		<status><?php echo $email; ?>Temp email</status>
-		<addFriend> <a href="#">Add Friend</a><br> </addFriend>
-		<addFriend2> <a href="#">Message</a> </addFriend2>
+		<username><?php echo $username; ?>Temp bc doesn't work <button class="button username" id="">Change username</button> </username>
+		
+		<email><?php echo $email; ?>Temp email <button class="button email" id="">Change username</button> </email>
+		<button class="button addFriend" id="">Add Friend</button>
+		<button class="button message" id="">Message</button>
 	
 </div>
 
