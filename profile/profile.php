3def8bc6143b9a447a212c78fbbbafdd2a01f562
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-10-27 08:49:37 +0000

    working on backend of profile

--- profile/profile.php
+++ profile/profile.php
@@ -23,155 +23,16 @@ else {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>Warcraft II</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../stylesheet.css">
+	<link rel="stylesheet" href="stylesheet.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-<!-- CSS Styling -->
-<style>
-	html, body {
-	background: url("../img/Texture.png") repeat;} 
 
-		.profile {
-		position:absolute;
-		top: 50%;
-		left: 50%;
-		padding: 40px 40px;
-		transform: translate(-50%, -50%);
-		width: 1200px;
-		height: 500px;
-		background-image: url("../img/wowDoge.jpg");
-		z-index: -999;
-
-	}
-
-	.profilePic{
-		position:absolute;
-		left: 50px;
-		top: 50px;
-		float:left;
-	}
-
-
-
-	.profile username {
-		position:absolute;
-		top: 30px;
-		left: 40%;
-		color: white;
-		font-size: 40px;
-
-
-	}
-	.userinfo{ 
-		position: static;
-	}
-
-	.profile email{
-		position:absolute;
-		top: 90px;
-		left: 40%;	
-		font-size: 18px;
-		text-align: center;
-		color: white;
-		font-size: 35px;
-	}
-	.email{
-		position:static;
-	}
-
-	.addFriend {
-		position:absolute;
-		left: 500px;
-		top: 200px;
-		font-size: 20px;
-	}
-
-	.message {
-		position:absolute;
-		left: 510px;
-		top: 250px;
-		font-size: 20px;
-	}
-
-
-.box {
-  position: absolute;
-  top: 35%;
-  left: 45%;
-  background: white;
-  padding: 10px 0px;
-
-
-  
-  text-align: center;
-}
-
-.button {
-  font-size: 1em;
-  width: 10px;
-  padding: 10px;
-  color: black;
-  cursor: pointer;
-  
-}
-.button:hover {
-  background: #8c8c8c;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.7);
-  transition: opacity 500ms;
-  visibility: hidden;
-  opacity: 0;
-}
-.overlay:target {
-  visibility: visible;
-  opacity: 1;
-}
-
-.popup {
-  margin: 150px auto;
-  padding: 20px;
-  background: #fff;
-  border-radius: 5px;
-  width: 30%;
-  position: relative;
-}
-
-
-.popup h2 {
-  margin-top: 0;
-  color: #333;
-  
-}
-.popup .close {
-  position: absolute;
-  top: 20px;
-  right: 30px;
-  transition: all 200ms;
-  font-size: 30px;
-  font-weight: bold;
-  text-decoration: none;
-  color: #333;
-}
-.popup .close:hover {
-  color: #999999;
-}
-.popup .content {
-  max-height: 30%;
-  overflow: auto;
-}
-
-</style>
 
 <!-- Nav Bar -->
 <div id="navbar"></div>
@@ -183,7 +44,7 @@ echo "</script>\n";
 ?>
 
 
-<!-- gray background -->
+<!--background -->
 <div class="background">
 
 </div>
@@ -223,13 +84,24 @@ echo "</script>\n";
 			<a class="close" href="#">&times;</a>
 			<div class="content">
 			
-				<?php 
+		<?php 
+		$host= "localhost";  //database host
+		$username="root";  //database username for log in
+		$userpass="ecs160web"; //database password for log in
+		$databasew="web"; //database schema name
+		$mysqli = new mysqli($host,$username,$userpass,$databasew);
+
+		if ($mysqli->connect_errno){
+		    echo "we have a problem";
+		}
 				echo " <form id=\"form\" method=\"post\">
-	  			<input type=\"text\" name=\"usrname\"><button  id=\"usr\">Update</button>username<br>
-				<input type=\"text\" name=\"firstname\"><button  id=\"\">Update</button>real name<br>
-				<input type=\"text\" name=\"firstname\"><button  id=\"\">Update</button>email<br>
+	  			<input type=\"text\" name=\"usrname\" placeholder= \"Username\"><button  id=\"usrname\">Update</button><br>
+				<input type=\"text\" name=\"firstname\" placeholder= \"Real name\"><button  id=\"firstname\">Update</button><br>
+				<input type=\"text\" name=\"email\" placeholder=\"Email\"><button  id=\"email\">Update</button><br>
 				</form> "
-				?> 
+
+		$mysqli->close();
+		?> 
 			
 
 			</div>
