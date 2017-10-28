679bf1d4f1fd922b38ce53857dd8d0fab48b4504
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-10-28 00:12:36 +0000

    fixed issues with username not in database when posting'

--- forum/forum.js
+++ forum/forum.js
@@ -99,7 +99,7 @@ function formatHtmlString(data, path, container)
 	var html_string = ' \
              	<a href="#"> \
             		<div class="jumbotron"> \
-              			<div class="col-sm-2"> <img align=left src="' + path + ' " alt= "' + path + ' " style="width:100px;height:100px;"> <p> ' + data[1] +' </p></div> \
+              			<div class="col-sm-2"> <img align=left src="' + path + ' " alt= "' + path + ' " style="width:100px;height:100px;"> <p> ' + username +' </p></div> \
               			<h3> ' + header + '</h3> \
               			<p> ' + content + ' </p> \
               			<footer> ' + date +' </footer> \
