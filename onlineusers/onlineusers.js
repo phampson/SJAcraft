dc74f262d735ee6283d6ecc2684bc5b63f6d2f01
Ubuntu <ubuntu@ip-172-26-13-143.us-west-2.compute.internal>
2017-10-29 18:34:01 +0000

    onlineusers.js now takes JSON

--- onlineusers/onlineusers.js
+++ onlineusers/onlineusers.js
@@ -1,44 +1,57 @@
-//$(document).ready(AddUser);
+$(document).ready(AddUsers(1));
+$(document).ready(AddUsers(2));
+
+// returns JSON array of logged in users 
+// type 1: game
+// type 2: web
 function GetUser(type)
 {
-//type 1 is game logged
-//type 2 is web logged
+	var obj;
 	$.ajax({
 		method: "POST",
 		url: "online.php",
 		data: { user_type: type },
 		success: function(data) {
-			var obj = jQuery.parseJSON(data);
-			console.log(obj);
+			obj = jQuery.parseJSON(data);
 		}
 	});
+	return obj;
 }
-GetUser(1);
 
-function AddUser() 
+// adds users to the html
+// type 1: game
+// type 2: web
+function AddUsers(type) 
 {
-	var container = document.getElementById("new_user_container_web");
+	if(type == 1) {
+		var container = document.getElementById("new_user_container_game");
+	}
+	else {
+		var container = document.getElementById("new_user_container_web");
+	}
 
 	// request	
-			var user_array = rec.split(",");
-			// Add User
-			for(i = 1; i < user_array.length; i++ ) {
-				var user_name = user_array[i];
-				console.log(user_name);
-				
-				var html_string = ' \
-			<div class="row"> \
-                                <div class="col-sm-2 col-sm-offset-1"> \
-                                        <img align=left src="../img/default.png" style="width:60px;height:60px" alt="Warcraft main picture"></img> \
-                                </div> \
-                                <div class="col-sm-4"> \
-                                        <h3>' + user_name + '</h3> \
-                                </div> \
-                                <div class="col-sm-5"> \
-                                        <img align=left src="../img/maps/plus.jpg" style="width:60px;height:60px" alt="Add Friend picture"></img> \
-                                </div> \
-                        </div> \
-			<br>'
-				container.insertAdjacentHTML('beforeend', html_string);
-			}
+	var users = GetUser(type);
+
+	// add users
+	console.log("Hello");
+	console.log(users);
+	for(var i in users) {
+		console.log(users[i]);
+		var html_string = ' \
+		<div class="row"> \
+			<div class="col-sm-2 col-sm-offset-1"> \
+				<img align=left src="../img/default.png" style="width:60px;height:60px" alt="Warcraft main picture"></img> \
+			</div> \
+			<div class="col-sm-4"> \
+				<h3>' + users[i] + '</h3> \
+			</div> \
+			<div class="col-sm-5"> \
+				<img align=left src="../img/maps/plus.jpg" style="width:60px;height:60px" alt="Add Friend picture"></img> \
+			</div> \
+		</div> \
+		<br>'
+
+		container.insertAdjacentHTML('beforeend', html_string);
+	}
 }
