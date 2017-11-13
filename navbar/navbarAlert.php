7fe371153430fcf1f5206855eb29e84fafd82c37
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-11-13 23:42:45 +0000

    navbar alert done

--- navbar/navbarAlert.php
+++ navbar/navbarAlert.php
@@ -7,9 +7,9 @@ if(isset($_SESSION['user_id'])) {
 	$query = 'select * from friendlist where interact_msgid!=newest_msgid and user_id = "'.$userid.'"';
         if ($result = $mysqli->query($query)) {
                 if($result->num_rows > 0) {
-                        echo 'false';
-                } else {
                         echo 'true';
+                } else {
+                        echo 'false';
                 }
         }
 }
