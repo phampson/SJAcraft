e1a888c2b7fdff8057a73142ebe4ab97d46ee9f5
HAOYU CHEN <chenhaoyu1995@gmail.com>
2017-11-13 15:18:56 -0800

    Get userid

--- navbar/navbarAlert.php
+++ navbar/navbarAlert.php
@@ -1,15 +1,15 @@
 <?php
 include('/home/ubuntu/ECS160WebServer/start.php');
 
-if(isset($_POST['user_id'])) {
-	$userid = $_POST['user_id'];
+if(isset($_SESSION['user_id'])) {
+	$userid = $_SESSION['user_id'];
 
 	$query = 'select * from friendlist where interact_msgid!=newest_msgid and user_id = "'.$userid.'"';
         if ($result = $mysqli->query($query)) {
                 if($result->num_rows > 0) {
-                        echo 'False';
+                        echo 'false';
                 } else {
-                        echo 'True';
+                        echo 'true';
                 }
         }
 }
