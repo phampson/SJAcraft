ed4f53ce1e1a885b7d0570f64e5bb0c18be0c35c
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-11-17 18:45:29 +0000

    ELO version 0.1

--- leaderboard/leaderboard.php
+++ leaderboard/leaderboard.php
@@ -3,12 +3,12 @@ include('/home/ubuntu/ECS160WebServer/start.php');
 
 if(isset($_POST))
 { 
-	$sql = 'SELECT username FROM user_info ORDER BY ELO LIMIT 10';
+	$sql = 'SELECT ELO, username FROM user_info ORDER BY ELO LIMIT 10';
  
         $result=$mysqli->query($sql) or die("project query fail");
         $arr = array();
         while ($row = $result->fetch_row()) {
-            $arr[] = $row;
+            $arr[$row[1]] = $row[0];
         }
         echo json_encode($arr);
 }
---
+++ signup/testmail.php
@@ -0,0 +1,3 @@
+<?php
+mail("ecs160test@gmail.com","tTest","test","From:ecs160test@gmail.com");
+?>
