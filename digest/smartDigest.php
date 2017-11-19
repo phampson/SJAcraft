4ca19bcf0cc6451a0db61586056e73e071fa90db
Ubuntu <ubuntu@ip-172-26-13-143.us-west-2.compute.internal>
2017-11-19 01:40:16 +0000

    created new files for emailing users

---
+++ digest/smartDigest.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once("/home/ubuntu/ECS160WebServer/start.php");
+include_once("/home/ubuntu/ECS160WebServer/digest/digestInclude.php");
+
+/*require '/home/ubuntu/ECS160WebServer/phpmailer/PHPMailer.php';
+require '/home/ubuntu/ECS160WebServer/phpmailer/SMTP.php';
+require '/home/ubuntu/ECS160WebServer/phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;*/
+
+// search through everyone that is subscribed to the post commented on and send them an email
+	$sql = "SELECT user_id FROM forum_digest WHERE post_id = '$ID'"; 
+	$query = $mysqli->query($sql);
+	$users = array();
+	
+
+	while($row = mysqli_fetch_array($query))
+	{	print_r($row);
+		$users[] = $row['user_id'];		
+	}
+	
+	$imploded_users = implode(',', $users);
+
+	$query = "SELECT * FROM user_info WHERE digest=1 AND id IN ($imploded_users)";
+	
+	sendDigest($query, "smartDigest.txt", -0.00555);
+
+	phpinfo();
+?>
