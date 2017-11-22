b6676ea48b5e2026501a84a565f1e4062d6434b6
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-11-22 05:08:17 +0000

    This commit adds a basic sound package page, it can upload files to the database. Wowee!

---
+++ downloadsoundpkg/downloadsoundpkg.php
@@ -0,0 +1,110 @@
+<?php
+include('/home/ubuntu/ECS160WebServer/start.php');
+
+if(isset($_SESSION['user_id'])){
+    $navpath = "../navbar/navbarlogged.html";
+} else {
+    $navpath = "../navbar/navbar.html";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Warcraft II</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../stylesheet.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<!-- Nav Bar -->
+<div id="navbar"></div>
+<?php
+echo "<script>\n";
+        echo '$("#navbar").load("' . $navpath . '")';
+echo "</script>\n";
+?>
+
+<!-- CDN gallery -->
+<h2 style="color: white; text-align: center;">Sound Packages Gallery</h2>
+
+<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+
+</div>
+    
+<!-- ********************************************************************* -->
+<?php
+	//Connect to the database
+	$host= "localhost";  //database host
+	$username="root";  //database username for log in
+	$userpass="ecs160web"; //database password for log in
+	$databasew="web"; //database schema name
+	$mysqli = new mysqli($host,$username,$userpass,$databasew);
+
+	if ($mysqli->connect_errno){
+	    echo "we have a problem";
+	}
+
+	$query = "select * from soundpkgs";
+	
+	if ($result = $mysqli->query($query)) {
+		$count = 0;
+	    while ($row = $result->fetch_assoc()) {
+			if($count % 4 == 0){
+				echo "<div class='row'>";		
+			}
+			$soundpkg_path = $row['soundpkg_path'];
+			$soundpkg_name = $row['soundpkg_name'];
+			//$displayName = $row['display_name'];
+			$uploaderID = $row['uploader'];
+		
+			$userNameQuery = $mysqli->query("SELECT * FROM user_info WHERE id=$uploaderID");
+			$uploaderName = ($userNameQuery->fetch_assoc())['username'];
+			echo "
+				<div class='col-sm-3'>
+					<div class='thumbnail'>
+						<img src='soundpkg/soundPic.png' alt='Fuck these stupid ass bugs' style='width:100%'>
+						<div class='caption'>
+							<p>$soundpkg_name</p>
+							<p>Uploaded by: $uploaderName</p>
+						</div>
+					<div style='text-align: center'><button><a href=$soundpkg_path download>Download</a></button></div>
+				</div>
+		
+				</div>";
+			if($count % 4 == 3){
+				echo "</div>";		
+			}
+			$count = $count + 1;
+	    }
+
+	    $result->close();
+	}
+	?>
+<!-- ********************************************************************* -->
+
+
+		<!-- Block to upload sound packages -->
+		<div class="col-sm-3">
+			<div class="thumbnail">
+				<img src="../img/maps/plus.jpg" alt="Insert Sound" style="width:100%">
+				<div class="caption">
+					<!-- Form that calls upload.php -->
+					<form action="upload.php" method="post" enctype="multipart/form-data">
+						Select sound package to upload:
+							<input type="file" name="fileToUpload" id="fileToUpload"> 
+							<input type="submit" value="Upload Image" name="submit">
+					</form>
+				</div>
+			</div>
+		</div>
+</div>
+
+
+</body>
+</html>
+
---
+++ downloadsoundpkg/upload.php
@@ -0,0 +1,55 @@
+<?php
+//db connection
+include('/home/ubuntu/ECS160WebServer/start.php');
+if ($_SESSION['user_id']==NULL) {
+	echo 'Please <a href="../login/login.html">log in</a> to upload a sound package';
+	exit;
+}
+
+//uploading file
+$target_dir = "soundpkg/";
+    
+// you will copy file into this directory
+$name = basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+//fileToUpload is the name from html file, [“name”] is an attribute of $_FILES instance. It also have attribute [“size”] below
+$uploadOk = 1;
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 1000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file to server
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        //define uploder, and create the insert query
+        $uploader = $_SESSION['user_id'];   
+	    $sql = "insert into soundpkgs (soundpkg_name, soundpkg_path, display_name, uploader) values('$name', '$target_file', 'Parker is a sir', '$uploader' )";
+
+        //try to do the insert 
+        if($mysqli->query($sql) or die($mysqli->error)) {
+		    echo "label success";
+        } else {
+		    echo $sql."label failed ";
+	    }
+        
+        //on success you will redirect back to soundpkg.php
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    	header("location: soundpkg.php");
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
