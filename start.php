595b4cd5f25e3b5f1a9e22e64dbea4f5ea8127bf
Marco Gomez <marcocoa@users.noreply.github.com>
2017-11-26 00:27:30 -0800

    Beautification

--- start.php
+++ start.php
@@ -4,28 +4,27 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 //Start Database Connection
-$host= "localhost";
-$username="root";
-$userpass="ecs160web";
-$databasew="web";
-$mysqli = new mysqli($host,$username,$userpass,$databasew);
-if ($mysqli->connect_errno){
+$host      = "localhost";
+$username  = "root";
+$userpass  = "ecs160web";
+$databasew = "web";
+$mysqli    = new mysqli($host, $username, $userpass, $databasew);
+if ($mysqli->connect_errno) {
     echo "Error connecting to Database";
     exit;
- }
+}
 
 //Begin Session
 session_start();
 
 //Delete session after inactivity and log user out
 if (!isset($_SESSION['EXPIRES']) || $_SESSION['EXPIRES'] < time()) {
-    if(isset($_SESSION['user_id'])){
-        $id = $_SESSION['user_id'];
-        $sql = "update user_info set web_logged='false' where id=$id";
+    if (isset($_SESSION['user_id'])) {
+        $id    = $_SESSION['user_id'];
+        $sql   = "update user_info set web_logged='false' where id=$id";
         $query = $mysqli->query($sql);
     }
     session_destroy();
     $_SESSION = array();
 }
-
 ?>
