d3b146445904e7ebc0dd27a7b7230d39c22b34ad
Ubuntu <ubuntu@warcarft2.org>
2017-11-27 02:55:17 +0000

    Implemented zip upload, where zip is stored in packages db and each file is correctly stored in corresponding directory and its info stored in Package_contents. Initial functionalities of download/upload page and display page were implemented

--- downloadCMaps/upload.php
+++ downloadCMaps/upload.php
@@ -3,22 +3,94 @@
 include('/home/ubuntu/ECS160WebServer/start.php');
 
 if ($_SESSION['user_id']==NULL) {
-	echo 'Please <a href="../login/login.html">log in</a> to upload a map!';
-	exit;
+	header("Location: ../login/login.html");
 }
 
-//uploading file
 $target_dir = "cMapPkgs/"; 
+
+$userID = $_SESSION["user_id"];
 
 // you will copy file into this directory
 $name = basename($_FILES["fileToUpload"]["name"]);
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
+// check to see if file exists
+/*if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    exit;
+}*/
+
+// move file from tmp location to its new location
+$tempLocation = $_FILES["fileToUpload"]["tmp_name"];
+$newLocation = $target_dir . $name;
+move_uploaded_file($tempLocation,$newLocation);
+
+// dummy insert to get next ID (folder var, maybe a bad name choice)
+$mysqli->query("INSERT INTO packages () VALUES ()");
+$folderQuery = "SELECT MAX(id) as 'id' FROM packages";
+$folder = (($mysqli->query($folderQuery))->fetch_assoc())['id'];
+
+
+// insert it into the database
+$packageQuery = "UPDATE packages SET
+                 uploader = '$userID', 
+                 name     = '$name', 
+                 filepath = '$newLocation' 
+                 WHERE id='$folder'";
+$mysqli->query($packageQuery) or die("Error inserting package: " . $mysqli->error);
+
 //unzip the map package!!! 
 $zip = new ZipArchive;
-$res = $zip->open($target_file );
+$res = $zip->open($newLocation);
 if ($res === TRUE) {
-  $zip->extractTo($target_dir);
+  $zip->extractTo($target_dir."/$folder/");
+
+  for ($i=0; $i<$zip->numFiles; $i++) {
+    $item = $zip->getNameIndex($i);
+
+    $item = pathinfo($item);
+    print_r($item);
+    if (isset($item['extension'])) {
+      $extension = $item['extension'];
+      $name = $item['basename'];
+      $path = $item['dirname'];
+      echo $extension . "<br>";
+
+      if ($extension == "map") {
+        $thumbnail = "cMapPkgs/$folder/" . $item["filename"] . ".png";
+        $output    = exec("../dlc/png $thumbnail cMapPkgs/$folder/$name");
+
+        $numPlayers     = substr($output, 0, 1);
+        $displayName    = substr($output, 1, strlen($output) - 1);
+
+        $mapUploadQuery = "INSERT INTO package_contents 
+                           (id, name, filepath, type, map_thumbnail, num_players, display_name) 
+                           VALUES 
+                           ('$folder', '$name', 'cMapPkgs/$folder/$name',
+                            0, '$thumbnail', '$numPlayers', '$displayName')";
+
+        $mysqli->query($mapUploadQuery) or die ("bad map upload");
+        // to-do: create thumbnail, add it to the query ^^
+      } elseif ($extension == "png") {
+        $tileSetUploadQuery = "INSERT INTO package_contents 
+                               (id, name, filepath, type)
+                               VALUES
+                               ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 1)";
+        $mysqli->query($tileSetUploadQuery) or die ("bad map upload");
+      } elseif ($extension == "mid") {
+        $soundUploadQuery = "INSERT INTO package_contents
+                             (id, name, filepath, type)
+                             VALUES
+                             ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 2)";
+        $mysqli->query($soundUploadQuery) or die ("bad map upload");
+      } else {
+        // unwanted file, delete it (this probably doesn't work, fix later)
+        // or reject the whole upload? :thinking:
+        exec("rm $name");
+      }
+    } // end if isset(extension)
+  } // end for
+
   $zip->close();
   echo 'YEEAAHHH BOI!';
 } else {
@@ -26,6 +98,7 @@ if ($res === TRUE) {
   exit;
 }
 
+/*
 //fileToUpload is the name from html file, [“name”] is an attribute of $_FILES instance. It also have attribute [“size”] below
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -77,6 +150,6 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+}*/
 ?>
 
