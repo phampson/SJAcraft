58cc0282d4803ea89f48e1a11d554033261a1bbb
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-11-27 07:23:05 +0000

    formatted php files in login/multi folder

--- login/multi/client.php
+++ login/multi/client.php
@@ -1,14 +1,16 @@
 <?php
-    if (!extension_loaded('curl')) {
-        echo 'failed';
-    } else {
-        echo 'loaded';
-    }
-error_reporting(E_ALL); ini_set('display_errors', '1');
-$name = "XU";
-$url ="http://34.214.129.0/login/multi/index.php?name=XU";
-$client=curl_init($url);
-curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
-$response=curl_exec($client);
+if (!extension_loaded('curl')) {
+    echo 'failed';
+} 
+else {
+    echo 'loaded';
+}
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$name   = "XU";
+$url    = "http://34.214.129.0/login/multi/index.php?name=XU";
+$client = curl_init($url);
+curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($client);
 echo $response;
 ?>
