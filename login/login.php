fa57e8c2c7a85bf12347fe8d2b579f29bdccf66e
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-11-27 06:50:38 +0000

    Formatted php files from login folder

--- login/login.php
+++ login/login.php
@@ -1,56 +1,56 @@
 <?php
 //start session and start logout timer
 error_reporting(0);
-if(!isset($_SESSION)) 
-{ 
-    session_start(); 
-} 
-$_SESSION['EXPIRES'] = time() + 3600;//Change this value to increase or decrease the logout value
+if (!isset($_SESSION)) {
+    session_start();
+}
+$_SESSION['EXPIRES'] = time() + 3600; //Change this value to increase or decrease the logout value
 include('start.php');
-error_reporting(E_ALL); ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-if (isset($_POST['username'])&&isset($_POST['password']))
-{
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-
-	$sql = 'select * from user_info where username="'.$username.'" and password="'.$password.'"';
-
-	$query = $mysqli->query($sql);
-	if ($query->num_rows > 0) {
-		$fetch = $query->fetch_assoc();
-		$email_verify = $fetch['email_verify'];
-		$reset_pass = $fetch['reset_pass'];
-		$id = $fetch['id'];
-		$_SESSION['user_id'] = $fetch['id'];
-		if ($email_verify == 0){//wrong for now for testing. should be ==0
-			deliver_response("email not verified");
-		}
-		else if ($reset_pass == 1){
-			deliver_response("password not reset");
-		}	
-		else {
-			//echo "Welcome! ";
-			//echo $fetch['username'];
-			$update = 'update user_info set web_logged="true" where id="'.$id.'"';
-			if ($mysqli->query($update)) {
-				//header("Location: ../profile/profile.php");
-				deliver_response("success");
-			}
-		}
-	}
-
-	else {
-		deliver_response("username or password invalid");
-	}
-}else
-{deliver_response("post method crushed");}
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $sql = 'select * from user_info where username="' . $username . '" and password="' . $password . '"';
+    
+    $query = $mysqli->query($sql);
+    if ($query->num_rows > 0) {
+        $fetch               = $query->fetch_assoc();
+        $email_verify        = $fetch['email_verify'];
+        $reset_pass          = $fetch['reset_pass'];
+        $id                  = $fetch['id'];
+        $_SESSION['user_id'] = $fetch['id'];
+        if ($email_verify == 0) { //wrong for now for testing. should be ==0
+            deliver_response("email not verified");
+        } 
+        else if ($reset_pass == 1) {
+            deliver_response("password not reset");
+        } 
+        else {
+            //echo "Welcome! ";
+            //echo $fetch['username'];
+            $update = 'update user_info set web_logged="true" where id="' . $id . '"';
+            if ($mysqli->query($update)) {
+                //header("Location: ../profile/profile.php");
+                deliver_response("success");
+            }
+        }
+    }
+    else {
+        deliver_response("username or password invalid");
+    }
+} 
+else {
+    deliver_response("post method crushed");
+}
 
 function deliver_response($mess)
 {
-	$response['message']=$mess;
-	$json_response=json_encode($response);
-	echo $json_response; 
+    $response['message'] = $mess;
+    $json_response       = json_encode($response);
+    echo $json_response;
 }
 
 ?>
--- login/logout.php
+++ login/logout.php
@@ -2,12 +2,12 @@
 
 include('/home/ubuntu/ECS160WebServer/start.php');
 
-if(isset($_SESSION['user_id'])){
-	$id = $_SESSION['user_id'];
-	$sql = "update user_info set web_logged='false' where id=$id";
-	$query = $mysqli->query($sql);
+if (isset($_SESSION['user_id'])) {
+    $id    = $_SESSION['user_id'];
+    $sql   = "update user_info set web_logged='false' where id=$id";
+    $query = $mysqli->query($sql);
 }
-
+    
 session_destroy();
 header("Location: ../index.php");
 ?>
