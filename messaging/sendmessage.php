12f13999b2356b1903776c0c905f37a5a18a4271
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-11-27 13:23:51 +0000

    formatted php files in messaging folder

--- messaging/sendmessage.php
+++ messaging/sendmessage.php
@@ -1,21 +1,21 @@
 <?php
 $frid = $_POST["frid"];
 $usid = $_POST["usid"];
-$msg = $_POST["msg"];
+$msg  = $_POST["msg"];
 include('/home/ubuntu/ECS160WebServer/start.php');
-$query = 'INSERT INTO message (sender,message_content,receiver,message_date) VALUES ('.(int)$usid.',"'.$msg.'" ,'.(int)$frid.',NOW())';
-function update_newestmsg($user_id,$friend_id,$mysqli){
-    $message_sql = 'select * from message where (sender='.$user_id.' and receiver='.$friend_id.') or (sender='.$friend_id.' and receiver = '.$user_id.') ORDER BY message_date DESC';
+$query = 'INSERT INTO message (sender,message_content,receiver,message_date) VALUES (' . (int) $usid . ',"' . $msg . '" ,' . (int) $frid . ',NOW())';
+function update_newestmsg($user_id, $friend_id, $mysqli)
+{
+    $message_sql   = 'select * from message where (sender=' . $user_id . ' and receiver=' . $friend_id . ') or (sender=' . $friend_id . ' and receiver = ' . $user_id . ') ORDER BY message_date DESC';
     $message_query = $mysqli->query($message_sql);
-    if($newest_msg = $message_query->fetch_assoc()){
-        $newest_msgid = $newest_msg['message_id'];
-        $update_newest_sql = 'update friendlist set interact_msgid='.$newest_msgid.',newest_msgid='.$newest_msgid.' where user_id='.$user_id.' and friend_id='.$friend_id.';update friendlist set newest_msgid='.$newest_msgid.' where user_id='.$friend_id.' and friend_id='.$user_id.';';
+    if ($newest_msg = $message_query->fetch_assoc()) {
+        $newest_msgid      = $newest_msg['message_id'];
+        $update_newest_sql = 'update friendlist set interact_msgid=' . $newest_msgid . ',newest_msgid=' . $newest_msgid . ' where user_id=' . $user_id . ' and friend_id=' . $friend_id . ';update friendlist set newest_msgid=' . $newest_msgid . ' where user_id=' . $friend_id . ' and friend_id=' . $user_id . ';';
         $mysqli->multi_query($update_newest_sql);
     }
 }
-if($result = $mysqli->query($query))
-{
-    update_newestmsg($usid,$frid,$mysqli);
+if ($result = $mysqli->query($query)) {
+    update_newestmsg($usid, $frid, $mysqli);
     echo $frid;
 }
 ?>
--- messaging/showmessage.php
+++ messaging/showmessage.php
@@ -6,47 +6,47 @@ include('/home/ubuntu/ECS160WebServer/start.php');
 function DisplayMessage($friend_id, $user_id)
 {
     global $mysqli;
-    $query = 'SELECT * FROM message WHERE (sender = "'.(int)$user_id.'" AND receiver = '.$friend_id.') OR (sender = '.$friend_id.' AND receiver = "'.(int)$user_id.'")';
-    if ($result = $mysqli->query($query)){
-        while($row = $result->fetch_assoc()){
-	    $content = $row['message_content'];
+    $query = 'SELECT * FROM message WHERE (sender = "' . (int) $user_id . '" AND receiver = ' . $friend_id . ') OR (sender = ' . $friend_id . ' AND receiver = "' . (int) $user_id . '")';
+    if ($result = $mysqli->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $content  = $row['message_content'];
             $receiver = $row['receiver'];
-            $date = $row['message_date'];
-            $msgid = $row['message_id'];
-            $sql = 'SELECT * FROM user_info WHERE id = "'.(int)$user_id.'"';
-            $qry = $mysqli->query($sql);
-            $myrow = $qry -> fetch_assoc();
-            $myname = $myrow['username'];
-	    $myAvatar = $myrow['avatar_path'];
-	    if (is_null($myAvatar)) {
-	        $myPicturePath = "../img/profpic.png";
-	    }
-	    else {
-	        $myPicturePath = "../profile/$myAvatar";
-	    }
-            $sql = 'SELECT * FROM user_info WHERE id = "'.(int)$friend_id.'"';
-            $qry = $mysqli->query($sql);
-            $friendrow = $qry -> fetch_assoc();
-            $friendname = $friendrow['username'];
-	    $friendAvatar = $friendrow['avatar_path'];
-	    if (is_null($friendAvatar)) {
-	        $friendPicturePath = "../img/profpic.png";
-	    }
-	    else {
-	        $friendPicturePath = "../profile/$friendAvatar";
-	    }
+            $date     = $row['message_date'];
+            $msgid    = $row['message_id'];
+            $sql      = 'SELECT * FROM user_info WHERE id = "' . (int) $user_id . '"';
+            $qry      = $mysqli->query($sql);
+            $myrow    = $qry->fetch_assoc();
+            $myname   = $myrow['username'];
+            $myAvatar = $myrow['avatar_path'];
+            if (is_null($myAvatar)) {
+                $myPicturePath = "../img/profpic.png";
+            } 
+            else {
+                $myPicturePath = "../profile/$myAvatar";
+            }
+            $sql          = 'SELECT * FROM user_info WHERE id = "' . (int) $friend_id . '"';
+            $qry          = $mysqli->query($sql);
+            $friendrow    = $qry->fetch_assoc();
+            $friendname   = $friendrow['username'];
+            $friendAvatar = $friendrow['avatar_path'];
+            if (is_null($friendAvatar)) {
+                $friendPicturePath = "../img/profpic.png";
+            }
+            else {
+                $friendPicturePath = "../profile/$friendAvatar";
+            }
             if ($receiver == $friend_id) {
-	        echo "
+                echo "
 		<div class='row msgContainer base_sent'>
                     <div class='col-md-10 col-xs-10'>
                         <div id='$msgid' class='messages msg_sent'>";
-                if (preg_match("/^attachment_files\/.*/", $content, $matches))
-                {
+                if (preg_match("/^attachment_files\/.*/", $content, $matches)) {
                     echo "
                          <p><a href='$content'>$content</a></p>";
+                } 
+                else {
+                    echo "<p>$content</p>";
                 }
-                else
-                { echo "<p>$content</p>";}
                 echo "
                             <time>$myname | $date</time>
                         </div>
@@ -56,42 +56,42 @@ function DisplayMessage($friend_id, $user_id)
                     </div>
                 </div>
 				";
-	    }
-	    else {
-	        echo "
+            } 
+            else {
+                echo "
 		<div class='row msgContainer base_receive'>
                     <div class='col-md-2 col-xs-2 avatar'>
                         <img class='img-circle' src='$friendPicturePath' style='width:65px;' style='height:65px'>
                     </div>
                     <div class='col-xs-10 col-md-10'>
                         <div id='$msgid' class='messages msg_receive'>";
-                if (preg_match("/^attachment_files\/.*/", $content, $matches))
-                {
+                if (preg_match("/^attachment_files\/.*/", $content, $matches)) {
                     echo "
                          <p><a href='$content'>$content</a></p>";
+                } 
+                else {
+                    echo "<p>$content</p>";
                 }
-                else
-                { echo "<p>$content</p>";}
                 echo "
                             <time>$friendname | $date</time>
                         </div>
                     </div>
                 </div>";
-	    }
-	}
+            }
+        }
     }
-
+    
 }
 
-DisplayMessage($frid,$usid);
+DisplayMessage($frid, $usid);
 /***
-                if (preg_match("/php/i", "PHP is the web scripting language of choice.", $matches))
-                {
-                    echo "A match was found:" . $matches[0];
-                }
-                else
-                {
-                    echo "<p>$content</p>";
-                }
+if (preg_match("/php/i", "PHP is the web scripting language of choice.", $matches))
+{
+echo "A match was found:" . $matches[0];
+}
+else
+{
+echo "<p>$content</p>";
+}
 ***/
 ?>
