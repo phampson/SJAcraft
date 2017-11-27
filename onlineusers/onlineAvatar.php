840d13842896c838721d56915677064090c58678
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-11-27 15:35:26 +0000

    onlineusers php files formatted

--- onlineusers/onlineAvatar.php
+++ onlineusers/onlineAvatar.php
@@ -1,26 +1,22 @@
-<?php 
+<?php
 include('/home/ubuntu/ECS160WebServer/start.php');
 
-if(isset($_POST['user_type']))
-{
-	$type=$_POST['user_type'];
-	if($type==1)
-	{
-		$sql = 'SELECT avatar_path FROM user_info WHERE game_logged = "true"';
-	}
-	else if($type==2)
-	{
-		$sql = 'SELECT avatar_path FROM user_info WHERE web_logged = "true"';
-	}
-	$result=$mysqli->query($sql) or die("project query fail");
-	$arr = array();
-	while ($row = $result->fetch_row()) {
-	    $arr[] = $row;
-	}
-	echo json_encode($arr);
-}
-else 
-{
-	echo "failed";
+if (isset($_POST['user_type'])) {
+    $type = $_POST['user_type'];
+    if ($type == 1) {
+        $sql = 'SELECT avatar_path FROM user_info WHERE game_logged = "true"';
+    } 
+    else if ($type == 2) {
+        $sql = 'SELECT avatar_path FROM user_info WHERE web_logged = "true"';
+    }
+    $result = $mysqli->query($sql) or die("project query fail");
+    $arr = array();
+    while ($row = $result->fetch_row()) {
+        $arr[] = $row;
+    }
+    echo json_encode($arr);
+} 
+else {
+    echo "failed";
 }
 ?>
