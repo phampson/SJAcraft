37a3bf5b970d2a3af98b49686a96890c6f7707fc
Ubuntu <ubuntu@warcarft2.org>
2017-11-28 23:00:31 +0000

    Can now preview animations in custom map packages

--- downloadCMaps/displayCMap.php
+++ downloadCMaps/displayCMap.php
@@ -30,14 +30,15 @@ echo "</script>\n";
 ?>
 
 <h2 style="color: white; text-align: center;">Contents</h2>
+<div class='col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2'>
 
 <?php
 $id =  $_GET['id'];
 
 //gets the correct package based on the id passed to this page
 $query = "select * from packages where id = " . $id;
-
-if ($result = $mysqli->query($query)) {
+$result = $mysqli->query($query); 
+if ($result !== NULL && $result->num_rows !== 0) {
     //collect data about this id in the packages database
     $row = $result->fetch_assoc();
     $uploader = $row['uploader'];
@@ -45,11 +46,10 @@ if ($result = $mysqli->query($query)) {
     $packageFilePath = $row['filepath'];
 
     //First displays the maps
-    echo "<h2 style='color: white; text-align: center;'>Maps</h2>
-          <div class='col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2'>";
-
     $query = "select * from package_contents where id = " . $id . " AND type = 0";
-    if($result = $mysqli->query($query)){
+    $result = $mysqli->query($query);
+    if($result !== NULL && $result->num_rows !== 0){
+      echo "<h2 style='color: white; text-align: center;'>Maps</h2>";
       $count = 0;
       while ($row = $result->fetch_assoc()) {
         $name = $row['name'];
@@ -84,10 +84,45 @@ if ($result = $mysqli->query($query)) {
     }
 
     //Now display all images
-    echo "<h2 style='color: white; text-align: center;'>Images</h2>";
-
     $query = "select * from package_contents where id = " . $id . " AND type = 1";
-    if($result = $mysqli->query($query)){
+    $result = $mysqli->query($query);
+    if($result !== NULL && $result->num_rows !== 0){
+      echo "<h2 style='color: white; text-align: center;'>Images</h2>";
+      $count = 0;
+      while ($row = $result->fetch_assoc()) {
+        $name = $row['name'];
+        $filepath = $row['filepath'];
+        if ($count % 4 == 0) {
+          echo "<div class='row'>";
+        }
+
+        echo "
+                <div class='col-sm-3'>
+                    <div class='div1 thumbnail'>
+                        <img src='$filepath' alt='$filepath' style='width:100%'>
+                        <div class='caption'>
+                            <p>$name</p>
+                            <p>Uploaded by: <a href='../profile/profile.php?id=$uploader'>$uploader</a></p>
+                        </div>
+                    <div style='text-align: center'>
+                    </div>
+                </div>
+        
+                </div>";
+        if ($count % 4 == 3) {
+            echo "</div>";
+        }
+        $count = $count + 1;
+        }
+        echo "</div>";
+    }
+
+
+    //Now display all animations
+    $query = "select * from package_contents where id = " . $id . " AND type = 3";
+    $result = $mysqli->query($query);
+    if($result !== NULL && $result->num_rows !== 0){
+      echo "<h2 style='color: white; text-align: center;'>Animations</h2>";
       $count = 0;
       while ($row = $result->fetch_assoc()) {
         $name = $row['name'];
@@ -118,10 +153,10 @@ if ($result = $mysqli->query($query)) {
     }
 
     //Now display all sounds     
-    echo "<h2 style='color: white; text-align: center;'>Sound</h2>";
-
     $query = "select * from package_contents where id = " . $id . " AND type = 2";
-    if($result = $mysqli->query($query)){
+    $result = $mysqli->query($query);
+    if($result !== NULL && $result->num_rows !== 0){
+      echo "<h2 style='color: white; text-align: center;'>Sound</h2>";
       $count = 0;
       while ($row = $result->fetch_assoc()) {
         
@@ -157,6 +192,10 @@ if ($result = $mysqli->query($query)) {
         $count = $count + 1;
         }
     }
+} else {
+  echo "
+    <h2 style='color: white; text-align: center;'>No Package Found :(</h2>
+  ";
 }
 ?>
 
@@ -165,3 +204,4 @@ if ($result = $mysqli->query($query)) {
 
 </body>
 </html>
+
--- downloadCMaps/upload.php
+++ downloadCMaps/upload.php
@@ -14,6 +14,7 @@ $userID = $_SESSION["user_id"];
 $name        = basename($_FILES["fileToUpload"]["name"]);
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
+// check to see if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     exit;
@@ -36,7 +37,7 @@ $packageQuery = "UPDATE packages SET
                  uploader = '$userID', 
                  name     = '$name', 
                  filepath = '$newLocation' 
-                 WHERE id='$folder'";
+                 WHERE id = '$folder'";
 $mysqli->query($packageQuery) or die("Error inserting package: " . $mysqli->error);
 
 //unzip the map package!!! 
@@ -53,6 +54,7 @@ if ($res === TRUE) {
         if (isset($item['extension'])) {
             $extension = $item['extension'];
             $name      = $item['basename'];
+            $basename  = $item['filename'];
             $path      = $item['dirname'];
             echo $extension . "<br>";
 
@@ -70,25 +72,46 @@ if ($res === TRUE) {
                             0, '$thumbnail', '$numPlayers', '$displayName')";
                 
                 $mysqli->query($mapUploadQuery) or die("bad map upload");
-            } 
-            elseif ($extension == "png") {
-                $tileSetUploadQuery = "INSERT INTO package_contents 
+            } elseif ($extension == "png") {
+		        // check for .dat, if it's there then it's an animation
+		        if (file_exists("cMapPkgs/$folder/$path/$basename.dat")) {
+                    list($width,$height,$type,$attr) = getimagesize("cMapPkgs/$folder/$path/$name");
+                    $size = $width . "x" . $width;
+
+                    //split into individual frames
+                    exec("convert -crop $size cMapPkgs/$folder/$path/$name cMapPkgs/$folder/c_%d.png");
+                    
+                    // repage so it's not a super tall gif
+                    for ($j=0; file_exists("cMapPkgs/$folder/c_$j.png"); $j++) {
+                        exec("convert -page $size+0+0 cMapPkgs/$folder/c_$j.png cMapPkgs/$folder/c_$j.png");
+                    }
+
+                    exec("convert -dispose Background -delay 20 -loop 0 cMapPkgs/$folder/c_*.png cMapPkgs/$folder/$basename.gif");
+                    exec("rm cMapPkgs/$folder/c_*.png");
+                    
+                    $animationInsertQuery = "INSERT INTO package_contents
+                                             (id, name, filepath, type)
+                                             VALUES
+                                             ('$folder', '$name', 'cMapPkgs/$folder/$basename.gif', 3)";
+                    $mysqli->query($animationInsertQuery) or die("lol the gifmaker thing messed up." . $mysqli->error());
+		        } else {
+		        // else it's a tileset
+		            $tileSetUploadQuery = "INSERT INTO package_contents 
                                (id, name, filepath, type)
                                VALUES
                                ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 1)";
-                $mysqli->query($tileSetUploadQuery) or die("bad map upload");
-            } 
-            elseif ($extension == "mid") {
+                    $mysqli->query($tileSetUploadQuery) or die("bad map upload");
+                }
+            } elseif ($extension == "mid" || $extension == "mp3" || $extension == "wav") {
                 $soundUploadQuery = "INSERT INTO package_contents
                              (id, name, filepath, type)
                              VALUES
                              ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 2)";
                 $mysqli->query($soundUploadQuery) or die("bad map upload");
-            } 
-            else {
+            } else {
                 // unwanted file, delete it (this probably doesn't work, fix later)
                 // or reject the whole upload? :thinking:
-                exec("rm $name");
+                //exec("rm $name");
             }
         } // end if isset(extension)
     } // end for
@@ -103,4 +126,5 @@ else {
 }
 
 ?>
+
 
