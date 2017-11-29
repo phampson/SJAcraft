938e1689ade8ec0d3461ed59c3ed33908ebef0b2
Ubuntu <ubuntu@warcarft2.org>
2017-11-29 03:27:29 +0000

    Private/public package repos implemented, weird redirect bug to fix.

---
+++ profile/packagerepo.php
@@ -0,0 +1,241 @@
+<?php
+
+include('/home/ubuntu/ECS160WebServer/start.php');
+
+if (isset($_SESSION['user_id'])) {
+    $navpath   = "../navbar/navbarlogged.html";
+    $repoOwner = $_SESSION['user_id']; 
+} else {
+    $navpath = "../navbar/navbar.html";
+}
+
+if (isset($_GET['id'])) {
+    $repoOwner = $_GET['id'];
+}
+
+$viewingOwnRepo = $repoOwner == $_SESSION['user_id'];
+
+function displayPackages($query, $ownRepo, $private) {
+    $mysqli = $GLOBALS["mysqli"];
+
+    if ($result = $mysqli->query($query)) {
+	    $count = 0;
+        while ($row = $result->fetch_assoc()) {
+	        if($count % 4 == 0){
+		        echo "<div class='row'>";		
+	        }
+	        $filepath         = $row['filepath'];
+	        $pname            = $row['name'];
+	        $displayName      = $row['name'];
+	        $uploaderID       = $row['uploader'];
+	        $packageID        = $row['id'];
+	        $userNameQuery    = $mysqli->query("SELECT * FROM user_info WHERE id=$uploaderID");
+	        $packagePathQuery = $mysqli->query("select * from packages where id = $packageID");
+	        $imagePathQuery   = $mysqli->query("select * from package_contents where id = $packageID AND type = 0");
+	        
+            if(!$userNameQuery){	
+	            throw new Exception("Error in Database query");
+            } else {
+	            $uploaderName = ($userNameQuery->fetch_assoc())['username'];
+            }	
+            if(!$packagePathQuery){	
+	            throw new Exception("Error in Database query");
+            } else {
+	            $packagePath = "../downloadCMaps/" . ($packagePathQuery->fetch_assoc())['filepath'];
+            }
+
+            if(!$imagePathQuery){	
+	            throw new Exception("Error in Database query");
+            } else {
+	            $imagePath = "../downloadCmaps/" . ($imagePathQuery->fetch_assoc())['map_thumbnail'];
+            }
+            
+            echo "
+		            <div class='col-sm-4'>
+                			<div class='div2 thumbnail'>";
+            if(file_exists($imagePath)){
+	            echo "<img src='../downloadCMaps/$imagePath' alt='RATSSSS' style='width:100%'>";
+            } else {
+	            echo "<img src='../downloadCMaps/package.png' alt='RATSSSS' style='width:100%'>";
+            }
+
+	        echo"	<div class='caption'>
+			        <p>$displayName</p>
+			        <p>Uploaded by: <a href='../profile/profile.php?id=$uploaderID'>$uploaderName</a></p>
+		        </div>
+	        <div style='text-align: center'>
+		        <button><a href='$packagePath' download>download</a></button>
+		        <button><a href='../downloadCMaps/displayCMap.php?id=$packageID'>Preview</a></button>";
+		    
+		    if ($ownRepo) {
+                echo "<p>Change public/private: </p>";
+                echo "<input type='checkbox' name='$packageID" . "[]' value='switch'>";
+                echo "<p>Delete map: </p>";
+                echo "<input type='checkbox' name='$packageID" . "[]' value='delete'>";
+                
+                if ($private == true) {
+                    echo "<br>Share with a friend only:
+				          <select name='$packageID" . "[]' value='share'>
+				            <option value='share'></option>";
+                    $friends = $mysqli->query("SELECT friend_id FROM friendlist WHERE user_id=" . $_SESSION["user_id"]);
+                    while ($friend = $friends->fetch_assoc()) {
+                        $friendID       = $friend["friend_id"];
+                        $friendQuery    = $mysqli->query("SELECT username FROM user_info WHERE id=$friendID");
+                        $friendFetch    = $friendQuery->fetch_assoc();
+                        $friendUserName = $friendFetch["username"];
+                        echo "<option value='$friendID'>$friendUserName</option>";
+                    }
+                    echo '</select>';
+                    
+                    $friendsSharedWith = $mysqli->query("SELECT shared_user FROM package_sharing WHERE id='$packageID'");
+                    
+                    echo "<br>Unshare with a friend: <br> <select name='$packageID" . "[]' value='share'><option value='unshare'></option>";
+                    while ($friend = $friendsSharedWith->fetch_assoc()) {
+                        $friendID = $friend["shared_user"];
+                        phpConsole($friendID);
+                        $friendQuery    = $mysqli->query("SELECT username FROM user_info WHERE id=$friendID");
+                        $friendFetch    = $friendQuery->fetch_assoc();
+                        $friendUserName = $friendFetch["username"];
+                        
+                        echo "<option value='$friendID'>$friendUserName</option>";
+                    }
+                    echo '</select>';
+                }
+		    }
+		    
+		    echo"				
+	        </div>
+	        </div>
+            </div>";
+            if ($count % 4 == 3) {
+                echo "</div>";
+            }
+        $count = $count + 1;
+        }
+
+    $result->close();
+    }
+}
+
+function displayUploadButton()
+{
+    $mysqli = $GLOBALS["mysqli"];
+    
+    echo '
+		<div class="row">
+		    <div class="col-sm-8 col-xs-8 col-sm-offset-2">
+				<div class="div2 thumbnail">
+					
+					<img src="../img/maps/plus.jpg" alt="Map1" style="width:100%">
+					<center><div class="caption">
+						<form action="uploadpackage.php" method="post" enctype="multipart/form-data"><p>
+						    <input type="radio" name="private" value="1">
+						    Private <br>
+						    <input type="radio" name="private" value="0" checked>
+						    Public <br>
+						    Select package to upload:
+						    <input type="file" name="fileToUpload" id="fileToUpload">
+				            Share with a friend only:
+				            <select>
+				            <option value=""></option>"';
+    $friends = $mysqli->query("SELECT friend_id FROM friendlist WHERE user_id=" . $_SESSION["user_id"]);
+    while ($friend = $friends->fetch_assoc()) {
+        $friendID       = $friend["friend_id"];
+        $friendQuery    = $mysqli->query("SELECT username FROM user_info WHERE id=$friendID");
+        $friendFetch    = $friendQuery->fetch_assoc();
+        $friendUserName = $friendFetch["username"];
+        echo "<option value='$friendID'>$friendUserName</option>";
+    }
+    echo '			    </select> 
+		                    <h2><input class="btn-simple btn-sm" type="submit" value="Upload Package" name="submit"></h2>
+					  </p>  </form>
+					</div></center>
+				    </a>
+			    </div>
+			</div>
+		</div>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Warcraft II</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../stylesheet.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- Nav Bar -->
+<div id="navbar"></div>
+<?php
+
+echo "<script>\n";
+echo '$("#navbar").load("' . $navpath . '")';
+echo "</script>\n";
+?>
+
+<!-- CDN gallery -->
+<div class='div1 col-xs-12 col-sm-8 col-xs-offset-0 col-sm-offset-2' id='border-gold'>
+<h2>Custom Map Gallery</h2><hr>
+<center>
+	<form action='dlc.php' method='get' onsubmit='encodeInput(this)'><h2>
+		<input style='color:black;' class='btn-sm' type='text' name='searchTerm' placeholder="Search term">
+		<input class='btn-simple btn-sm' type='submit' value='Search'>
+		<select style='color:black;' class='btn-sm' name='sort'>
+			<option disabled selected value>Sort By</option>
+			<option value='players'>players</option>
+			<option value='uploader'>uploader</option>
+			<option value='name'>name</option>
+			<option value='date'>date</option>
+		</select></h2>		
+	</form><hr>
+</center>
+
+<script type='text/javascript'>
+	function encodeInput(form)
+	{
+		form.elements['searchTerm'].value = encodeURIComponent(form.elements['searchTerm'].value);
+	}
+</script>
+
+<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+<?php
+
+if ($viewingOwnRepo) {
+    displayUploadButton();
+    
+    echo "<form method='post' action='changepackagesettings.php' class='col-xs-12'>";
+    
+    echo "<div class='row'>";
+    echo "<h2>Public Repo</h2><hr>";
+    $query = "select * from packages where uploader=$repoOwner and private=0";
+    displayPackages($query, true, false);
+    echo "</div>";
+    
+    echo "<div class='row'>";
+    echo "<h2>Private Repo</h2><hr>";
+    $query = "select * from packages where uploader=$repoOwner and private=1";
+    displayPackages($query, true, true);
+    echo "</div>,";
+    
+    echo "<h2><input class='btn-simple btn-sm' type='submit' value='Apply Map Changes'></h2>";
+    echo "</form>";
+    
+} else {
+    echo "<div class='row'>";
+    echo "<h2>Public Repo</h2><hr>";
+    
+    $query = "select * from packages where uploader=$repoOwner and private=0";
+    displayPackages($query, false, false);
+}
+
+
+?>
+
+</div>
+</body>
+</html>
---
+++ profile/uploadpackage.php
@@ -0,0 +1,134 @@
+<?php
+//db connection
+include('/home/ubuntu/ECS160WebServer/start.php');
+
+if ($_SESSION['user_id'] == NULL) {
+    header("Location: ../login/login.html");
+}
+
+$private = $_POST['private'];
+
+$target_dir = "../downloadCMaps/cMapPkgs/";
+
+$userID = $_SESSION["user_id"];
+
+// you will copy file into this directory
+$name        = basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+// check to see if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    exit;
+    echo "Exit??";
+}
+
+// move file from tmp location to its new location
+$tempLocation = $_FILES["fileToUpload"]["tmp_name"];
+$newLocation  = $target_dir . $name;
+move_uploaded_file($tempLocation, $newLocation);
+
+// dummy insert to get next ID (folder var, maybe a bad name choice)
+$mysqli->query("INSERT INTO packages () VALUES ()");
+$folderQuery = "SELECT MAX(id) as 'id' FROM packages";
+$fq          = $mysqli->query($folderQuery);
+$fetchFolder = $fq->fetch_assoc();
+$folder      = $fetchFolder['id'];
+
+// insert it into the database
+$packageQuery = "UPDATE packages SET
+                 uploader = '$userID', 
+                 name     = '$name', 
+                 filepath = '$newLocation', 
+		 private  = '$private'
+                 WHERE id = '$folder'";
+$mysqli->query($packageQuery) or die("Error inserting package: " . $mysqli->error);
+
+//unzip the map package!!! 
+$zip = new ZipArchive;
+$res = $zip->open($newLocation);
+if ($res === TRUE) {
+    $zip->extractTo($target_dir . "/$folder/");
+    
+    for ($i = 0; $i < $zip->numFiles; $i++) {
+        $item = $zip->getNameIndex($i);
+        
+        $item = pathinfo($item);
+        print_r($item);
+        if (isset($item['extension'])) {
+            $extension = $item['extension'];
+            $name      = $item['basename'];
+            $basename  = $item['filename'];
+            $path      = $item['dirname'];
+            echo $extension . "<br>";
+
+            if ($extension == "map") {
+                $thumbnail = "../downloadCMaps/cMapPkgs/$folder/" . $item["filename"] . ".png";
+                $output    = exec("../dlc/png $thumbnail ../downloadCMaps/cMapPkgs/$folder/$path/$name");
+                
+                $numPlayers  = substr($output, 0, 1);
+                $displayName = substr($output, 1, strlen($output) - 1);
+                
+                $mapUploadQuery = "INSERT INTO package_contents 
+                           (id, name, filepath, type, map_thumbnail, num_players, display_name) 
+                           VALUES 
+                           ('$folder', '$name', 'cMapPkgs/$folder/$path/$name',
+                            0, '$thumbnail', '$numPlayers', '$displayName')";
+                
+                $mysqli->query($mapUploadQuery) or die("bad map upload");
+            } elseif ($extension == "png") {
+		        // check for .dat, if it's there then it's an animation
+		        if (file_exists("../downloadCMaps/cMapPkgs/$folder/$path/$basename.dat")) {
+                    list($width,$height,$type,$attr) = getimagesize("../downloadCMaps/cMapPkgs/$folder/$path/$name");
+                    $size = $width . "x" . $width;
+
+                    //split into individual frames
+                    exec("convert -crop $size ../downloadCMaps/cMapPkgs/$folder/$path/$name ../downloadCMaps/cMapPkgs/$folder/c_%d.png");
+                    
+                    // repage so it's not a super tall gif
+                    for ($j=0; file_exists("../downloadCMaps/cMapPkgs/$folder/c_$j.png"); $j++) {
+                        exec("convert -page $size+0+0 ../downloadCMaps/cMapPkgs/$folder/c_$j.png ../downloadCMaps/cMapPkgs/$folder/c_$j.png");
+                    }
+
+                    exec("convert -dispose Background -delay 20 -loop 0 ../downloadCMaps/cMapPkgs/$folder/c_*.png ../downloadCMaps/cMapPkgs/$folder/$basename.gif");
+                    exec("rm ../downloadCMaps/cMapPkgs/$folder/c_*.png");
+                    
+                    $animationInsertQuery = "INSERT INTO package_contents
+                                             (id, name, filepath, type)
+                                             VALUES
+                                             ('$folder', '$name', 'cMapPkgs/$folder/$basename.gif', 3)";
+                    $mysqli->query($animationInsertQuery) or die("lol the gifmaker thing messed up." . $mysqli->error());
+		        } else {
+		        // else it's a tileset
+		            $tileSetUploadQuery = "INSERT INTO package_contents 
+                               (id, name, filepath, type)
+                               VALUES
+                               ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 1)";
+                    $mysqli->query($tileSetUploadQuery) or die("bad map upload");
+                }
+            } elseif ($extension == "mid" || $extension == "mp3" || $extension == "wav") {
+                $soundUploadQuery = "INSERT INTO package_contents
+                             (id, name, filepath, type)
+                             VALUES
+                             ('$folder', '$name', 'cMapPkgs/$folder/$path/$name', 2)";
+                $mysqli->query($soundUploadQuery) or die("bad map upload");
+            } else {
+                // unwanted file, delete it (this probably doesn't work, fix later)
+                // or reject the whole upload? :thinking:
+                //exec("rm $name");
+            }
+        } // end if isset(extension)
+    } // end for
+    
+    $zip->close();
+    echo 'YEEAAHHH BOI!';
+    header("Location: uploadpackage.php");
+} 
+else {
+    echo 'RATSSSSSS!';
+    exit;
+}
+
+?>
+
+
