569454be4e097ece8533feced81c225f19d7b64f
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-11-30 01:17:57 +0000

    Started developing fron-tend and functionality for support pages where bugs are reported

---
+++ support/submit.php
@@ -0,0 +1,16 @@
+<h1>Submit</h1>
+<hr>
+<form action="" id="bugform">
+    Title:<br>
+    <input type="text" name="title" value="Mickey">
+    <br>
+    Details:<br>
+    <textarea rows="4" cols="50" name="details">
+    Enter Text Here ...
+    </textarea>
+    <input type="submit" value="Submit">
+</form>
+
+<?php  
+    
+?>
