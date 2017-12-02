a46763e5248ff7a194c7b722026d6e11bfc1df7a
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-12-02 00:56:41 +0000

    reads in bugs dynamically

--- support/support.php
+++ support/support.php
@@ -20,7 +20,7 @@ else {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Warcraft II-Support Page</title>
+	<title>Warcraft II Support</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="../stylesheet.css">
@@ -39,36 +39,42 @@ echo "</script>\n";
 ?>
 
 <div class="div1 container" id="border-gold">
-<h1>Support Page</h1><hr>
+<h1>Support</h1><hr>
 	<div class="panel-group" id="accordion">
 
 <?php 
+	
+	$sql = 'SELECT title, details, resolved FROM support';
+	$result = $mysqli->query($sql) or die("project query fail");
+    	
+    $pos = 1;
+	while ($row = $result->fetch_row()) {
+        $title  		= $row[0];
+        $details	    = $row[1];
+        $resolved 		= $row[2];
+        
+        if($resolved == "true") {
+        	$status = '<span class="glyphicon glyphicon-ok"></span>';
+        } else {
+        	$status = '<span class="glyphicon glyphicon-hourglass"></span>';
+        }
+    
         echo "<div class='panel panel-success'>
 		  		<div class='panel-heading'>
 				    <div class='panel-title'>
-				    	<a data-toggle='collapse' data-parent='#accordion' href='#collapse1'>
-				        <center>Bug Title (Resolved)</center></a>
+				    	<a data-toggle='collapse' data-parent='#accordion' href='#collapse$pos'>
+				        <center>$title $status</center></a>
 				    </div>
 		    	</div>
-		    	<div id='collapse1' class='panel-collapse collapse'>
-				    <div class='panel-body'>In this paragraph, you would put the text describing the bug</div>
+		    	<div id='collapse$pos' class='panel-collapse collapse'>
+				    <div class='panel-body'>$details</div>
                     <div class='text-center'>
                         <button class='btn btn-default'><a href='support.php'>Resolve</a></button>
                     </div>
 			    </div>
 	  		</div>";
-	  		
-	  	echo "<div class='panel panel-danger'>
-		  		<div class='panel-heading'>
-				    <div class='panel-title'>
-				    	<a data-toggle='collapse' data-parent='#accordion' href='#collapse2'>
-				        <center>For Backend (In Progress)</center></a>
-				    </div>
-		    	</div>
-		    	<div id='collapse2' class='panel-collapse collapse'>
-				    <div class='panel-body'>You will likely make a loop of these echo statements when iterating through database. This post assumes you can't see the delete and resolve buttons since you are not admin</div>
-			    </div>
-	  		</div>";
+	  	$pos++;
+	}
 	  		
 	  	echo "</div>";
             
