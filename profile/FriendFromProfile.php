13aad9814dbdd670c0321db2ca7f32de756e1a25
Ubuntu <ubuntu@ip-172-26-10-31.us-west-2.compute.internal>
2017-12-03 16:38:56 +0000

    Finish request

--- profile/FriendFromProfile.php
+++ profile/FriendFromProfile.php
@@ -53,7 +53,7 @@ $mysqli->multi_query($addnewfriend);*/
 $user1      = $_GET["id"];
 $user2      = $_SESSION["user_id"];
 $addFriend1 = "INSERT INTO friendlist (user_id,friend_id) VALUES ('$user1','$user2')";
-$addFriend2 = "INSERT INTO friendlist (user_id,friend_id) VALUES ('$user2','$user1')";
+$addFriend2 = "INSERT INTO friendlist (user_id,friend_id,request) VALUES ('$user2','$user1',1)";
 
 $mysqli->query($addFriend1) or die("Add friend 1");
 $mysqli->query($addFriend2) or die("Add friend 2");
