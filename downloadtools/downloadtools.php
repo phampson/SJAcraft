0ca661b48676bcb8fc6c72b2c708be9a2b8b8646
Ubuntu <ubuntu@warcarft2.org>
2017-12-04 20:04:37 +0000

    Gives most up-to-date files

--- downloadtools/downloadtools.php
+++ downloadtools/downloadtools.php
@@ -43,7 +43,7 @@ echo "</script>\n";
 		</div>
 				    			   
 		<div style="align: center; position: relative;" class="button container col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0" id="downloadButton">
-			<center><a href="../gamefiles/ECS160Tools-week4.zip" download="ToolsApp"><button style="margin-bottom:20px;" type="button" class="btn btn-fancy-download"></button></a><center>
+			<center><a href="../gamefiles/Tools.zip" download="ToolsApp"><button style="margin-bottom:20px;" type="button" class="btn btn-fancy-download"></button></a><center>
 		</div>
 	</div>
 
