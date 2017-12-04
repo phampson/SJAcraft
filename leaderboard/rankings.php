4a8f30a48e66944955e0273f0484682f8d77bce8
Ubuntu <ubuntu@ip-172-26-15-128.us-west-2.compute.internal>
2017-12-04 06:25:29 +0000

    added word break in rankings.php

--- leaderboard/rankings.php
+++ leaderboard/rankings.php
@@ -124,7 +124,7 @@ echo "</script>\n";
             <tr> \
                 <td> \
                     <img align="left" src="../profile/'+ users[i].avatar_path +'" style=width:60px;height:60px;"> \
-                    <h3><a href="http://' + "<?php
+                    <h3><a style="word-wrap: break-word" href="http://' + "<?php
 echo $_SERVER['HTTP_HOST'];
 ?>" + '/profile/profile.php">' + users[i].name + '</a></h3> \
                 </td> \
